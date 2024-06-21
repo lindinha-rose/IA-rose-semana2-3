6f31bb8611f76540227f83d93e7317a3be89333d
rose <107115789+lindinha-rose@users.noreply.github.com>
2024-06-21 08:59:48 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const caixaResultado = document.querySelector(".caixa-resultado");
+
+
+const perguntas = [
+    {
+        enunciado: "Assim que saiu da escola você se depara com uma nova tecnologia,um chat que consegue responder todasas dúvidas que uma pessoa  pode ter, ele também gera imagem e áudios hiper-realistas. Qual o primeiro pensamento? ",
+        alternativas: [
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
+        ]
+    },
+    {
+        enunciado: "Com a descoberta desta tecnologia, chamada Inteligência Artificial, uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre esta tecnologia.",
+        alternativas: [
+            "Ultiliza uma ferramenta de busca na internet que ultiliza IA para que ela ajuda a encontrar informações relevantes parao trabalho e explique numa linguagem que facilite o entender sobre esta tecnologia. No fim de uma aula ela pede que vocẽ escreva um trabalho sobre o uso de IA em sala de aula. Qual atitude vocẽ toma?",
+            "alternativa 2"
+        ]
+
+
+    },
+    {
+        enunciado: "pergunta 2",
+        alternativas: [
+            "alternativa 1",
+            "alternativa 2"
+        ]
+
+
+    },
+    
+];
